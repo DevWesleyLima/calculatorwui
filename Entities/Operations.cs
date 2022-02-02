9cc28f3d0442e5a7b323921927b018e8cffb7831
Wesley Lima <wsouza1992@hotmail.com>
2022-02-02 21:32:37 +0000

    The Project has started

---
+++ Entities/Operations.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Globalization;
+
+namespace CalculatorWUI.Entities
+{
+    internal class Operations
+    {
+        public double Number1 { get; set; }
+        public double Number2 { get; set; }
+        public string Operation { get; set; }
+
+        public Operations()
+        {
+            Number1 = 0;
+            Number2 = 0;
+            Operation = String.Empty;
+        }
+
+        public Operations(int number1, int number2, string operation)
+        {
+            Number1 = number1;
+            Number2 = number2;
+            Operation = operation;
+        }
+     
+        public double Operator()
+        {  
+            return Operation switch
+            {
+                "+"  => Number1 + Number2,
+                "-"  => Number1 - Number2,
+                "*"  => Number1 * Number2,
+                "/"  => Number1 / Number2,
+                "+%" => Number1 + (Number1 * (Number2 / 100)),
+                "-%" => Number1 - (Number1 * (Number2 / 100)),
+                _ => 0.00,
+            };            
+        }
+        
+        public string StrOperations()
+        {
+            return Operation switch
+            {
+                "+" => "Sum",
+                "-" => "Subtraction",
+                "*" => "Multiplication",
+                "/" => "Division",
+                "+%" => "Sum Percentage",
+                "-%" => "Subtraction Percentage",
+                _ => "",
+            };
+        }
+
+        public override string ToString()
+        {
+
+            return "Operation: " + StrOperations() 
+                + '\n'
+                + "Result: " + Operator().ToString("0,0.00", CultureInfo.InvariantCulture);
+        }
+
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using CalculatorWUI.Entities;
+
+namespace CalculatorWUI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Calculator 
+            Console.WriteLine("Welcome to your calculator program!!!");
+            Console.WriteLine("*************************************");
+
+            Console.Write("Type the 1st number:");
+            int number1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Type the operator:");
+            string operation = Console.ReadLine();
+
+            Console.Write("Type the 2nd number:");
+            int number2 = int.Parse(Console.ReadLine());            
+
+            Operations calculator = new Operations(number1, number2, operation);
+
+            Console.WriteLine(calculator);
+
+            //Do another operation
+
+            //
+        }
+    }  
+}
